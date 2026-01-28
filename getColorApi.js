531806f669447e4ed80349983134c7438deb4899
Dimitris John Raptis <olrandir@gmail.com>
2026-01-28 18:22:25 +0200

    Revert "fixes + move  utils to typescript"
    This reverts commit b34e345c5a3b88d0ea9898325f365b6ff02f38cb.

---
+++ getColorApi.js
@@ -0,0 +1,23 @@
+async function getColorApi(callback) {
+  const url = "http://colormind.io/api/";
+  const data = {
+    model : "default",
+  }
+
+  const http = new XMLHttpRequest();
+
+  http.onreadystatechange = function() {
+    let palette = []
+    if (http.readyState == 4 && http.status == 200) {
+      palette = JSON.parse(http.responseText).result;
+    } else if (http.readyState == 4) {
+      console.error("Error fetching color palette: " + http.status + " " + http.statusText);
+    }
+    if (callback) {
+      callback(palette);
+    }
+  }
+
+  http.open("POST", url, true);
+  http.send(JSON.stringify(data));
+};
